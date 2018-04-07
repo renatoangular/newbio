65c99b8e8c87c517f2d15009e49c34226db847a2
Renato Nunez <212marines@gmail.com>
2018-04-07 15:57:23 -0400

    unignored files

---
+++ server/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  AUTH0_DOMAIN: 'biol.auth0.com', // e.g., kmaida.auth0.com
+  AUTH0_API_AUDIENCE: 'https://biol.auth0.com/api/v2/', // e.g., 'http://localhost:8083/api/'
+  MONGO_URI: process.env.MONGO_URI || 'mongodb://monica:monica111@ds153948.mlab.com:53948/renatotasklist', // || 
+  //MONGO_URI: 'mongodb://localhost/mean',
+  NAMESPACE: 'http://myapp.com/roles' // e.g., http://myapp.com/roles
+};
+
+ 
